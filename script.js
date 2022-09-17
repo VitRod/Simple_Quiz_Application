eafd9e4e74c0acdd5ce74d1e087fd4fd39f499e8
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-17 07:12:04 +0300

    added

--- script.js
+++ script.js
@@ -13,29 +13,37 @@ var questions = [
         question: "The loop keyword of java is?",
         choices: ["Byte", "While", "Double", "FLoat"],
         correctAnswer: 1,
-      },
-      {
+    },
+    {
         question: "Java does not define which type modifier?",
         choices: ["Auto", "Extern", "Register", "all of the above"],
         correctAnswer: 3,
-      },
-      {
+    },
+    {
         question: "	Which one of the following is access keyword?",
         choices: ["public", "private", "protected", "all of the above"],
         correctAnswer: 3,
-      },
-      {
+    },
+    {
         question:
-          "Which one of the following languages is pure object oriented language?",
+            "Which one of the following languages is pure object oriented language?",
         choices: ["java", "c++", "object pascal", "c"],
         correctAnswer: 0,
-      },
-      {
+    },
+    {
         question: "	Which one of the following is not a java keyword?",
         choices: ["break", "class", "static", "study"],
         correctAnswer: 3,
-      },
-
-
+    },
+    {
+        question: "POP is a/an",
+        choices: [
+            "Object oriented programming",
+            "Object optional programming",
+            "Optional object programming",
+            "None of these",
+        ],
+        correctAnswer: 0,
+    },
 
 ];
