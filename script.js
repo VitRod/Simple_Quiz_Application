e6a2abc28cb866695e88193a753ae4cf9a312f9e
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-17 06:44:02 +0300

    ar questions = [

---
+++ script.js
@@ -0,0 +1,9 @@
+var questions = [
+    {
+      question: "Which one of the following is a java keyword?",
+      choices: ["switch", "while", "public", "void", "all of the above"],
+      correctAnswer: 2,
+    },
+
+
+];
