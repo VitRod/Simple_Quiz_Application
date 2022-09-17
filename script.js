5d245f161085dfb4d9d0208d598c59174d8fec32
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-17 06:45:42 +0300

    {     question: "Java declaration statement must end with?",     choices: ["A comma", "a semicolon", "a colon", "fullstop"],     correctAnswer: 1,   },

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var questions = [
     {
-      question: "Which one of the following is a java keyword?",
-      choices: ["switch", "while", "public", "void", "all of the above"],
-      correctAnswer: 2,
+        question: "Which one of the following is a java keyword?",
+        choices: ["switch", "while", "public", "void", "all of the above"],
+        correctAnswer: 2,
     },
 
+
+    {
+        question: "Java declaration statement must end with?",
+        choices: ["A comma", "a semicolon", "a colon", "fullstop"],
+        correctAnswer: 1,
+    },
 
 ];
